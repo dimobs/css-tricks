9b279bc8dc35d8b0a9ddfd53342930eda8e9fe0a
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2023-04-28 16:46:46 +0300

    m

--- library lamp/script copy.js
+++ library lamp/script copy.js
@@ -25,8 +25,34 @@ const reducer = (state = initialState, action) => {
   }
 };
 
+// redux
+// redux.createStore
+// redux.createStore(reducer)
+// redux.createStore(reducer).getState()
+// event Listener
+// redux.createStore(reducer).subscribe() 
+// event Listener
+// redux.createStore(reducer).subscribe(console.log(redux.createStore(reducer).getState())) 
+// redux.createStore(reducer).dispatch(const reducer = (state = initialState, action) => {
+//   switch(action.type) {
+//     case BUY_CAKE: 
+//     return {
+//       ...state,
+//       numOfCake: state.numOfCake - 1
+//     }
+
+//     default: return state
+//   }
+// })
+
+
 const store = createStore(reducer);
-console.log('this is whole state', store);
+console.log('this is whole state', store['@@observable']);
+// dispatch: [Function: dispatch],
+//   subscribe: [Function: subscribe],
+//   getState: [Function: getState],
+//   replaceReducer: [Function: replaceReducer],
+//   '@@observable': [Function: observable]
 console.log('Initail state', store.getState());
 console.log('Incremer state', store.getState());
 const unsubscribe = store.subscribe(() => console.log('Update state', store.getState()));
