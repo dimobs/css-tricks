4b123238211c79b3c02704ff5d5b9956e9a74379
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2023-04-28 16:17:55 +0300

    m

--- library lamp/script copy.js
+++ library lamp/script copy.js
@@ -1,33 +1,36 @@
 const redux = require('redux');
-const createStore = redux.createStore
+const createStore = redux.createStore;
 
 const BUY_CAKE = 'BUY_CAKE';
-
-function buyCake() {
-  return {
-    type: BUY_CAKE,
-    info: 'First redux action'
-  }
-}
-
 const initialState = {
   numOfCake: 10
 }
 
+function buyCake() {
+  return {
+    type: BUY_CAKE, 
+    info: 'Redux action'
+  }
+};
+
 const reducer = (state = initialState, action) => {
   switch(action.type) {
-    case BUY_CAKE: return {
-      ...state,  
+    case BUY_CAKE: 
+    return {
+      ...state,
       numOfCake: state.numOfCake - 1
     }
 
     default: return state
   }
-}
+};
 
-const store = createStore(reducer)
-console.log('Initial state', store.getState());
+const store = createStore(reducer);
+console.log('this is whole state', store);
+console.log('Initail state', store.getState());
+console.log('Incremer state', store.getState());
 const unsubscribe = store.subscribe(() => console.log('Update state', store.getState()));
+console.log('Initail state', store.getState());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
