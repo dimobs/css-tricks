7d377a68adfbad11fde8231db1528c36fdee761d
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2023-06-03 13:47:02 +0300

    readFile

--- green login form/script.js
+++ green login form/script.js
@@ -1,8 +1,16 @@
 const fs = require('fs');
-const zlib = require('zlib');
+// const zlib = require('zlib');
 
-const gzip =  zlib.createGzip();
-const readStream = fs.createReadStream('./input.txt');
-const writeStream = fs.createWriteStream('./output.txt')
+// const gzip =  zlib.createGzip();
+// const readStream = fs.createReadStream('./input.txt');
+// const writeStream = fs.createWriteStream('./output.txt')
 
-readStream.pipe(gzip).pipe(writeStream);
+// readStream.pipe(gzip).pipe(writeStream);
+
+let data = fs.readdir('../', 'utf8', (err, data) =>{
+    if(err) {
+        console.log(err);
+        return;
+    }
+    console.log(data)
+});
