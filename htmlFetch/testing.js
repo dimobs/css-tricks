dd41a6aa7c70bc02e1bdc0f0e4e1c3273db1c5ac
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2023-11-02 23:29:56 +0200

    update

---
+++ htmlFetch/testing.js
@@ -0,0 +1,18 @@
+const fruits = ['apple', 'banana', 'cherry', 'date' ];
+
+function findFruitIndex(fruit) {
+    for (let i = 0; i < fruits.length; i++) {
+        if(fruits[i] == fruit){
+            continue
+        }
+    }
+    if(true){
+        return true
+    }
+  
+}
+
+const sercheFruit = 'cherry';
+console.log(findFruitIndex(sercheFruit));
+
+// console.log(fruitIndex);
