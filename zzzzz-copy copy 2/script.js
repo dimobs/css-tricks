e1173aa64f0be29e1f10a244c465dfb7953d907f
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2024-05-10 11:14:03 +0300

    inversion

--- zzzzz-copy copy 2/script.js
+++ zzzzz-copy copy 2/script.js
@@ -87,22 +87,22 @@
 // qSort()
 
 
-function countInversions(arr) {
-    let temp = new Array(arr.length);
-    return mergeSortAndCount(arr, temp, 0, arr.length - 1);
-}
+// function countInversions(arr) {
+//     let temp = new Array(arr.length);
+//     return mergeSortAndCount(arr, temp, 0, arr.length - 1);
+// }
 
-function mergeSortAndCount(arr, temp, left, right) {
-    let invCount = 0;
-    if (left < right) {
-        let mid = Math.floor((left + right) / 2);
+// function mergeSortAndCount(arr, temp, left, right) {
+//     let invCount = 0;
+//     if (left < right) {
+//         let mid = Math.floor((left + right) / 2);
 
-        invCount += mergeSortAndCount(arr, temp, left, mid); // Инверсии в лявата половина
-        invCount += mergeSortAndCount(arr, temp, mid + 1, right); // Инверсии в дясната половина
-        invCount += mergeAndCount(arr, temp, left, mid, right); // Инверсии между двата подмасива
-    }
-    return invCount;
-}
+//         invCount += mergeSortAndCount(arr, temp, left, mid); // Инверсии в лявата половина
+//         invCount += mergeSortAndCount(arr, temp, mid + 1, right); // Инверсии в дясната половина
+//         invCount += mergeAndCount(arr, temp, left, mid, right); // Инверсии между двата подмасива
+//     }
+//     return invCount;
+// }
 
 // function mergeAndCount(arr, temp, left, mid, right) {
 //     // Копиране на част от масива във временния масив
@@ -258,4 +258,45 @@ function mergeSortAndCount(arr, temp, left, right) {
     
 // }
 
+// function countingInversions(a){
+//     if(a.length<2) return [0,a];
+//     else{
+//         let mid,left,right,left_inv,right_inv,merged;
+//         let inversions=0;
+//         mid = Math.floor(a.length/2);                      //Finding middle index.
+        
+//         left = a.slice(0,mid);                           //Divides array into left and right.
+//         right = a.slice(mid,a.length);
+        
+//         left_inv = countingInversions(left);             //returns inversions and sorted array.
+//         right_inv = countingInversions(right);         
+        
+//         inversions = inversions+left_inv[0]+right_inv[0];
+        
+//         merged = merge(left_inv[1],right_inv[1]);              //returns inversions and merged array.
+//         inversions = inversions + merged[0];                   //Total number of inversions
+        
+//         return [inversions,merged[1]];
+//     }
+// }
+
+
+// console.log(countingInversions([50,20,5,70,30,15,11,18])[0]);
+
+arr = [0, 2, 5, 9, 7, 1, 6, 3, 8, 4];
+ 
+function getInvCount(arr){
+    let inv_count = 0;
+    for(let i=0; i<arr.length-1; i++){
+        for(let j=i+1; j<arr.length; j++){
+            console.log(arr[i], '=>', arr[j]);
+            if(arr[i] > arr[j]) console.log('has inversion');;
+            if(arr[i] > arr[j]) inv_count++;
+        }
+    }
+    return inv_count;
+}
+ 
+// function call
+console.log("Number of inversions are "+ getInvCount(arr));
 
