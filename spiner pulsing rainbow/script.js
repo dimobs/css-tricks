96dc7d885e5d465cf310d52651ae1913ce69cf66
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2024-09-28 01:58:50 +0300

    add search select text

---
+++ spiner pulsing rainbow/script.js
@@ -0,0 +1,36 @@
+let last = 0;
+let iterations = 10;
+
+function timeout() {
+  // log the time of this call
+  logline(new Date().getMilliseconds());
+  // if we are not finished, schedule the next call
+  if (iterations-- > 0) {
+    setTimeout(timeout, 0);
+  }
+}
+
+function run() {
+  // clear the log
+  const log = document.querySelector("#log");
+  while (log.lastElementChild) {
+    log.removeChild(log.lastElementChild);
+  }
+  // initialize iteration count and the starting timestamp
+  iterations = 10;
+  last = new Date().getMilliseconds();
+  // start timer
+  setTimeout(timeout, 0);
+}
+
+function logline(now) {
+  // log the last timestamp, the new timestamp, and the difference
+  const tableBody = document.getElementById("log");
+  const logRow = tableBody.insertRow();
+  logRow.insertCell().textContent = last;
+  logRow.insertCell().textContent = now;
+  logRow.insertCell().textContent = now - last;
+  last = now;
+}
+
+document.querySelector("#run").addEventListener("click", run);
---
+++ zzzzz-copy copy 9/script.js
@@ -0,0 +1,5 @@
+document.getElementById("search").oninput = function() {
+    const p = document.getElementById('card');
+    const r = new RegExp(`(${this.value})`, "gi");
+    p.innerHTML = p.textContent.replace(r, '<mark>$1</mark>')
+}
