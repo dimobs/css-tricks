21f965577aa3f50014fb72bad84447c5e1d9694f
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2024-10-15 00:23:23 +0300

    Burger close btn

--- zzzzz-copy copy 9/script.js
+++ zzzzz-copy copy 9/script.js
@@ -1,4 +1,4 @@
-document.getElementById("search").oninput = function() {
+document.getElementById("search").oninput = function () {
     const p = document.getElementById('card');
     const r = new RegExp(`(${this.value})`, "gi");
     p.innerHTML = p.textContent.replace(r, '<mark>$1</mark>')
